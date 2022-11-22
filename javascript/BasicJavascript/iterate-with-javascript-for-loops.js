64911af5ecb27c390fad05c84e631eb1ca67b8cf
Bertin <bertinlys@gmail.com>
2022-11-22 09:14:48 -0600

    here we go

---
+++ javascript/BasicJavascript/iterate-with-javascript-for-loops.js
@@ -0,0 +1,12 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+var ourArray = [];
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
